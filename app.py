004d36cd85878d8133aa8a8e3ad9d633354b4f03
jimmothy <jputnam1999@gmail.com>
2025-11-22 12:05:05 -0500

    adding start script, modifications to app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template_string, Request, Response
 from picamera2 import Picamera2
-import cv2, serial, time
+import cv2, serial, time, struct
 
 
 ser = serial.Serial(
@@ -55,18 +55,31 @@ app = Flask(__name__,
 
 @app.route('/')
 def hello_world():
-  return render_template_string('''Hi James World!\n
-
+  return render_template_string('''
+  <style>
+    .buttonContainer {
+      display: grid;
+      grid-template-columns: 200px 200px 200px;
+    }
+    button {
+        height:50px;
+    }
+  </style>
+  
   <img src="{{ url_for('video') }}" width="640" height="480" />
-  <div>
-    <button onclick="sendCommand()">Send Serial</button>
-    <button onclick="goForward()">Go Forward</button>
-    <button onmousedown="holdForward()" onmouseup="stop()">Hold Forward</button>
-    <button onclick="goBackwards()">Go Backwards</button>
-    <button onclick="turnLeft()">Turn Left</button>
-    <button onclick="turnRight()">Turn Right</button>
-    <button onclick="reset()">Reset</button>
+  <div class="buttonContainer">
+    <button onmousedown="sendCommand()">Send Serial</button>
+    <button onmousedown="goForward()" onmouseup="stop()">Hold Forward</button>
+    <button onmousedown="reset()">Reset</button>
+    <button onmousedown="turnLeft()" onmouseup="stop()">Turn Left</button>
+    <button> </button>
+    <button onmousedown="turnRight()" onmouseup="stop()">Turn Right</button>
     <button onclick="dock()">Dock</button>
+    <button onmousedown="goBackwards()" onmouseup="stop()">Go Backwards</button>
+    <button onclick="checkBattery()">check battery</button>
+  </div>
+  <div class="buttonContainer">
+    <button id="battery">{{percent}}</button
   </div>
 
 
@@ -82,13 +95,16 @@ def hello_world():
   function dock() {
     fetch("/dock", { method: "POST" });
   }
+  function checkBattery() {
+    fetch("/checkBattery", { method: "POST" })
+      .then(response => response.json())
+      .then(data => {document.getElementById("battery").innerText=data.message})
+      .catch(error => console.error("Error: ", error));
+  }
 
 
   function goForward() {
     fetch("/forward", { method: "POST" });
-  }
-  function holdForward() {
-    fetch("/forward/start", { method: "POST" });
   }
   function stop() {
     fetch("/stop", { method: "POST" });
@@ -106,6 +122,7 @@ def hello_world():
   ''')
 
 
+
 @app.route("/video")
 def video():
     return Response(generate_frames(),
@@ -117,6 +134,20 @@ def dock():
     print('docking')
     time.sleep(5)
     return "", 204
+
+@app.route("/checkBattery", methods=["POST"])
+def checkBattery():
+    ser.write(bytes([142,28]))
+    chargeBytes = ser.read(2)
+    charge = struct.unpack("<H", chargeBytes)[0]
+    time.sleep(0.5)
+
+    ser.write(bytes([142,29]))
+    capacityBytes = ser.read(2)
+    capacity = struct.unpack("<H", capacityBytes)[0]
+
+    percent = ("percent: " + str(charge/capacity))
+    return percent, 204
 
 
 @app.route("/reset", methods=["POST"])
@@ -141,9 +172,9 @@ def sing():
 
     return "", 204
 
-@app.route("/forward/start", methods=["POST"])
+@app.route("/forward", methods=["POST"])
 def forward_start():
-    ser.write(bytes([145,0x00,0x64,0x00,0x64]))
+    ser.write(bytes([145,0x01,0x00,0x01,0x00]))
     return "", 204
 
 @app.route("/stop", methods=["POST"])
@@ -151,37 +182,37 @@ def forward_stop():
     ser.write(bytes([145,0x00,0x00,0x00,0x00]))
     return "", 204
 
-@app.route("/forward", methods=["POST"])
-def forward():
-    ser.write(bytes([145,0x00,0x64,0x00,0x64]))
-    time.sleep(2)
-    ser.write(bytes([145,0x00,0x00,0x00,0x00]))
-    time.sleep(2)
-    return "", 204
+#@app.route("/forward", methods=["POST"])
+#def forward():
+#    ser.write(bytes([145,0x00,0x64,0x00,0x64]))
+#    time.sleep(2)
+#    ser.write(bytes([145,0x00,0x00,0x00,0x00]))
+#    time.sleep(2)
+#    return "", 204
 
 @app.route("/backwards", methods=["POST"])
 def backwards():
     ser.write(bytes([145,0xFF,0x9C,0xFF,0x9C]))
-    time.sleep(2)
-    ser.write(bytes([145,0x00,0x00,0x00,0x00]))
-    time.sleep(2)
     return "", 204
 
 @app.route("/left", methods=["POST"])
 def left():
     ser.write(bytes([145,0x00,0x64,0x00,0x00]))
-    time.sleep(1)
-    ser.write(bytes([145,0x00,0x00,0x00,0x00]))
-    time.sleep(2)
+#    time.sleep(1)
+#    ser.write(bytes([145,0x00,0x00,0x00,0x00]))
+#    time.sleep(2)
     return "", 204
 
 @app.route("/right", methods=["POST"])
 def right():
     ser.write(bytes([145,0x00,0x00,0x00,0x64]))
-    time.sleep(1)
-    ser.write(bytes([145,0x00,0x00,0x00,0x00]))
-    time.sleep(2)
+#    time.sleep(1)
+#    ser.write(bytes([145,0x00,0x00,0x00,0x00]))
+#    time.sleep(2)
     return "", 204
 
 if __name__ == '__main__':
-  app.run(host="0.0.0.0", port=5000, debug=True)
+    from waitress import serve
+    serve(app,host="0.0.0.0", port=5000)
+    #app.run(host="0.0.0.0", port=5000, debug=True)
+
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/jam/Poomba
+. ./venv/bin/activate
+python app.py
